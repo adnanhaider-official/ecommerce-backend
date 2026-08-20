6448042120175945525e6636356eb2ee641c2444
adnanhaider-official <adnan.haider.tech@gmail.com>
2026-08-20 11:40:50 +0500

    Add Auth Middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+import ApiError from "../utils/ApiError.js";
+import asyncHandler from "../utils/asyncHandler.js";
+
+const verifyJwt = asyncHandler(async (req, res, next) => {
+  const token =
+    req.cookies?.accessToken ||
+    req.header["authorization"]?.replace("Bearer ", "");
+
+  if (!token) {
+    throw new ApiError(401, "Unauthorized request");
+  }
+
+  const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+  const user = await User.findById(decodedToken._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!user) {
+    throw new ApiError(401, "Invalid access token");
+  }
+
+  req.user = user;
+
+  next();
+});
+
+export default verifyJwt;
