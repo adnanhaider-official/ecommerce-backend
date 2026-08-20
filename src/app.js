38a0a64509fe8e51867d3497edac33ab51fb74e2
adnanhaider-official <adnan.haider.tech@gmail.com>
2026-08-20 11:16:22 +0500

    Add Middleware in app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 // Middleware
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "20kb" }));
+app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 // Routes
 app.get("/", (req, res) => {
