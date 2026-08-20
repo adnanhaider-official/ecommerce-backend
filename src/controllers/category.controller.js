1ee2ff936e3b76ddaff7fe82fea401fa8e65f280
adnanhaider-official <adnan.haider.tech@gmail.com>
2026-08-20 13:45:07 +0500

    Add Pagination

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -29,17 +29,39 @@ const createCategory = asyncHandler(async (req, res) => {
 
 // Get All Categories
 const getAllCategory = asyncHandler(async (req, res) => {
-  const categories = await Category.find().sort({ name: 1 });
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
+  const skip = (page - 1) * limit;
+
+  const categories = await Category.find()
+    .sort({ name: 1 })
+    .skip(skip)
+    .limit(limit);
+
+  const totalCategories = await Category.countDocuments();
+
+  const totalPages = Math.ceil(totalCategories / limit);
 
   if (!categories) {
     throw new ApiError(404, "Categories not found");
   }
 
-  return res
-    .status(200)
-    .json(
-      new ApiResponse(200, categories, "Fetch All Categories Successfully")
-    );
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        categories,
+        pagination: {
+          currentPage: page,
+          limit,
+          totalPages,
+          totalCategories,
+        },
+      },
+      "Fetch All Categories Successfully"
+    )
+  );
 });
 
 export { createCategory, getAllCategory };
