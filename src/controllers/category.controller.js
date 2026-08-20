a50f324df71b469bcffbb50c8aa96af64c874bd7
adnanhaider-official <adnan.haider.tech@gmail.com>
2026-08-20 13:24:35 +0500

    get All Categories

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -3,6 +3,7 @@ import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import { Category } from "../models/category.model.js";
 
+// Create Categories
 const createCategory = asyncHandler(async (req, res) => {
   const { name, description } = req.body;
 
@@ -26,4 +27,19 @@ const createCategory = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, category, "Category Created Successfully"));
 });
 
-export { createCategory };
+// Get All Categories
+const getAllCategory = asyncHandler(async (req, res) => {
+  const categories = await Category.find().sort({ name: 1 });
+
+  if (!categories) {
+    throw new ApiError(404, "Categories not found");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, categories, "Fetch All Categories Successfully")
+    );
+});
+
+export { createCategory, getAllCategory };
--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { createCategory } from "../controllers/category.controller";
+import {
+  createCategory,
+  getAllCategories,
+} from "../controllers/category.controller";
 
 import { verifyJwt, isAdmin } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/create-category", verifyJwt, isAdmin, createCategory);
+
+router.get("/", getAllCategories);
 
 export default router;
