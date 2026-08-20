3a10a1099c207ed78b9bf752c78d86b5282acaa5
adnanhaider-official <adnan.haider.tech@gmail.com>
2026-08-20 11:11:15 +0500

    Add errorHandler Middleware

--- src/app.js
+++ src/app.js
@@ -2,9 +2,14 @@ import express from "express";
 
 const app = express();
 
+// Middleware
+
 // Routes
 app.get("/", (req, res) => {
   res.send("Hello Welcome backend Series");
 });
+
+// error middleware
+app.use(errorHandler);
 
 export { app };
