a8581f3e0e00b12a7aa0c9ff8b2fd1b34537e196
adnanhaider-official <adnan.haider.tech@gmail.com>
2026-08-20 12:29:17 +0500

    Add Product and Category Models

---
+++ src/models/product.model.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    stock: {
+      type: Number,
+      required: true,
+      min: 0,
+      default: 0,
+    },
+
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+
+    images: {
+      type: String,
+      required: true,
+    },
+
+    brand: {
+      type: String,
+      trim: true,
+      default: "",
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Product = mongoose.model("Product", productSchema);
